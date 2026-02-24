7d3defbca4cced166fc9e029e3ea216435512c98
Smriti Khanal <smritikhanal45@gmail.com>
2026-02-24 11:51:09 +0545

    chore: update project configuration and middleware

--- next.config.ts
+++ next.config.ts
@@ -5,55 +5,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'localhost',
-      },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
+        hostname: '**',
       },
       {
         protocol: 'https',
-        hostname: 'images.samsung.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.samsung.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn-1.azazie.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.gadgetbytenepal.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.myntassets.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'store.storeimages.cdn-apple.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.apple.com',
-      },
-      {
-        protocol: 'https',
-        hostname: '**.cloudinary.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
+        hostname: '**',
       },
     ],
   },
